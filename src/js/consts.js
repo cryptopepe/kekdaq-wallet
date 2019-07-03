2e3bed7ecbde45b71fff7fc77cb40bbabb549cf9
cyberpepe <kekronet@gmail.com>
2019-07-03 18:28:38 -0400

    Lower TX fees for our faster block times

--- src/js/consts.js
+++ src/js/consts.js
@@ -14,9 +14,11 @@ var MULTISIG_DUST_SIZE = UNIT * 2;
 var MIN_PRIME_BALANCE = 50000; //in satoshis ... == .0005
 var ASSET_CREATION_FEE_XCP = 0.5; //in normalized XCP
 var MAX_ASSET_DESC_LENGTH = 41; //42, minus a null term character?
-var FEE_FRACTION_REQUIRED_DEFAULT_PCT = .9; //0.90% of total order
-var FEE_FRACTION_PROVIDED_DEFAULT_PCT = 1; //1.00% of total order
-var FEE_FRACTION_DEFAULT_FILTER = .95;
+
+// Blockchain transaction creation fees lowered because coins are used whether or not the order is successful
+var FEE_FRACTION_REQUIRED_DEFAULT_PCT = 0.025; //0.025% of total order
+var FEE_FRACTION_PROVIDED_DEFAULT_PCT = 0.025; //0.025% of total order. These fees go to miners.
+var FEE_FRACTION_DEFAULT_FILTER = 0.01;
 var BTC_ORDER_MIN_AMOUNT = 0.01;
 
 var DEFAULT_NUM_ADDRESSES = 1; //default number of addresses to generate. Go with 1 for now to be more newbie friendly
